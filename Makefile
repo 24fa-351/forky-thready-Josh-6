c05728d6aef38079e6724bafbdadf896c597f55a
Josh-6 <Joshuabarpna@gmail.com>
2024-10-05 21:34:43 -0700

    Trying to get it to work when printing to results

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 fork: forky.c forky_patterns.c
 	gcc forky.c forky_patterns.c -o fork
+
+clean:
+	rm -f results.txt
--- forky_patterns.c
+++ forky_patterns.c
@@ -7,7 +7,7 @@
 
 void random_sleep(int n)
 {
-    srand(time(NULL) ^ (getpid() << 16));
+    srand((time(NULL)) ^ (getpid() << 16));
     int sleep_time = (rand() % 8) + 1;
     //printf("%d is sleeping for %d seconds!\n", n, sleep_time);
     sleep(sleep_time);
@@ -15,34 +15,70 @@ void random_sleep(int n)
 
 void pattern_one(int thing)
 {
-    for(int i = 1; i <= thing; i++)
+    FILE *fptr;
+    fptr = fopen("results.txt", "w");
+
+    pid_t pid;
+    for (int i = 1; i <= thing; i++)
     {
-        pid_t pid = fork();
-        
-        if(pid == 0) // equal to 0 means child
+        pid = fork();
+
+        if (pid == 0) // equal to 0 means child
         {
-            printf("Process %d (PID: %d) beginning\n", i, getpid());
+            fprintf(fptr,"Process %d (PID: %d) beginning\n", i, getpid());
             random_sleep(i);
-            printf("Process %d (PID: %d) exiting\n", i, getpid());
-            exit(0); 
+            fprintf(fptr,"Process %d (PID: %d) exiting\n", i, getpid());
+            exit(0);
         }
-        else if(pid > 0) // greater than 0 means parent
+        else if (pid > 0) // greater than 0 means parent
         {
-            // only have one parent and dont need to do anything to it
+            //printf("look at me im th parent! (PID: %d)\n", getpid());
         }
         else
         {
-            exit(1); // less than 0 means something went really wrong when creating fork
+            exit(1); // less than 0 means something went wrong when creating fork
         }
     }
 
-    for (int i = 1; i <= thing; i++) 
+    for (int i = 1; i <= thing; i++)
     {
         wait(NULL);
     }
+    fclose(fptr);
 }
 
 void pattern_two(int thing)
 {
+    FILE *ptr;
+    ptr = fopen("results.txt", "w");
 
+    pid_t pid;
+    for (int i = 1; i <= thing; i++)
+    {
+        pid = fork();
+        if (pid == 0)
+        {
+            //fprintf(ptr,"Process %d (PID: %d) beginning\n", i, getpid());
+            printf("Process %d (PID: %d) beginning\n", i, getpid());
+            random_sleep(i);
+            if (i < thing)
+            {
+                //fprintf(ptr,"Process %d (PID: %d) started Process %d (PID: %d)\n", i, getpid(), i + 1, getpid());
+                printf("Process %d (PID: %d) started Process %d (PID: %d)\n", i, getpid(), i + 1, getpid());
+            }
+        }
+        else if (pid > 0) // Parents actually can't be neglectful! 
+        {
+            //printf("Look at me im th parent! (PID: %d)\n", getpid());
+            wait(NULL);
+            //fprintf(ptr,"Process %d (PID: %d) exiting\n", i, getpid());
+            printf("Process %d (PID: %d) exiting\n", i, getpid());
+            exit(0); // Exit parent process after child finishes
+        }
+        else
+        {
+            exit(1);
+        }
+    }
+    fclose(ptr);
 }
