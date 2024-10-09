1ff02d5518e332625c7235d5b362bb4bdaf840be
Josh-6 <Joshuabarpna@gmail.com>
2024-10-08 19:59:33 -0700

    Pattern 2 now works, now gotta format result.txt correctly

--- forky_patterns.c
+++ forky_patterns.c
@@ -9,7 +9,7 @@ void random_sleep(int n)
 {
     srand((time(NULL)) ^ (getpid() << 16));
     int sleep_time = (rand() % 8) + 1;
-    //printf("%d is sleeping for %d seconds!\n", n, sleep_time);
+    // printf("%d is sleeping for %d seconds!\n", n, sleep_time);
     sleep(sleep_time);
 }
 
@@ -32,11 +32,11 @@ void pattern_one(int thing)
         int cpid = getpid();
         if (pid == 0) // equal to 0 means child
         {
-            printf("Process %d (PID: %d) beginning!\n", i, cpid);//getpid());
-            fprintf(stderr, "Process %d (PID: %d) beginning!\n", i, cpid);//getpid());
+            printf("Process %d (PID: %d) beginning!\n", i, cpid);          // getpid());
+            fprintf(stderr, "Process %d (PID: %d) beginning!\n", i, cpid); // getpid());
             random_sleep(i);
-            printf("Process %d (PID: %d) exiting\n", i, cpid);//getpid());
-            fprintf(stderr, "Process %d (PID: %d) exiting!\n", i, cpid);//getpid());
+            printf("Process %d (PID: %d) exiting\n", i, cpid);           // getpid());
+            fprintf(stderr, "Process %d (PID: %d) exiting!\n", i, cpid); // getpid());
             exit(0);
         }
         else if (pid > 0) // greater than 0 means parent
@@ -63,27 +63,40 @@ void pattern_two(int thing)
 
     dup2(fileno(ptr), fileno(stderr));
 
-    pid_t pid;
+    pid_t pid = fork();
     pid_t child_pid;
-    for (int i = 1; i < thing; i++)
+    for (int i = 1; i <= thing; i++)
     {
-        pid = fork();
-        //int parent_pid = getpid();
+        // pid = fork();
+        //  int parent_pid = getpid();
+
         if (pid == 0)
         {
-            fprintf(stderr, "Process %d (PID: %d) beginning\n", i,  getpid());
+            fprintf(stderr, "Process %d (PID: %d) beginning\n", i, getpid());
             printf("Process %d (PID: %d) beginning\n", i, getpid());
 
             random_sleep(i);
 
-            child_pid = fork();
+            if (i != thing)
+            {
+                child_pid = fork();
+            }
+
             if (child_pid == 0)
             {
-                fprintf(stderr, "Process %d (PID: %d) started Process %d (PID: %d)\n", i, getppid(), i + 1, getpid());
-                printf("Process %d (PID: %d) started Process %d (PID: %d)\n", i, getppid(), i + 1, getpid());
+                if (i != thing)
+                {
+                    fprintf(stderr, "Process %d (PID: %d) started Process %d (PID: %d)\n", i, getppid(), i + 1, getpid());
+                    printf("Process %d (PID: %d) started Process %d (PID: %d)\n", i, getppid(), i + 1, getpid());
+                }
+                else
+                {
+                    fprintf(stderr, "Process %d (PID: %d) exiting\n", i, getpid());
+                    printf("Process %d (PID: %d) exiting\n", i, getpid());
+                }
                 continue;
             }
-            else if (child_pid > 0)
+            else if (child_pid > 0 || i == thing)
             {
                 wait(NULL);
                 fprintf(stderr, "Process %d (PID: %d) exiting\n", i, getpid());
@@ -100,8 +113,6 @@ void pattern_two(int thing)
             // printf("Look at me im th parent! (PID: %d)\n", getpid());
             wait(NULL);
             
-            //fprintf(stderr, "Process %d (PID: %d) exiting\n", i, getpid());
-            //printf("Process %d (PID: %d) exiting\n", i, getpid());
             exit(0); // Exit parent process after child finishes
         }
         else
